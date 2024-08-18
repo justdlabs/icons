914561d43ab50f6baef7cbce4fa6571270314087
Irsyad A. Panjaitan <irsyad@parsinta.com>
2024-08-18 19:16:12 +0700

    icon: add new icon

---
+++ src/IconBrandYoutube.js
@@ -0,0 +1,19 @@
+import * as React from "react";
+const SvgIconBrandYoutube = (props) => (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    width={24}
+    height={24}
+    fill="none"
+    viewBox="0 0 24 25"
+    {...props}
+   className={`justd-icons ${props.className ? props.className : "size-4"}`} data-slot="icon" aria-hidden="true">
+    <path
+      fill="currentColor"
+      fillRule="evenodd"
+      d="M20.204 4.758c.904.28 1.615 1.101 1.857 2.147.437 1.893.439 5.845.439 5.845s0 3.952-.44 5.845c-.241 1.046-.952 1.867-1.856 2.147-1.637.508-8.204.508-8.204.508s-6.567 0-8.204-.508c-.904-.28-1.615-1.101-1.857-2.147C1.5 16.702 1.5 12.75 1.5 12.75s0-3.952.44-5.845c.241-1.046.952-1.867 1.856-2.147C5.433 4.25 12 4.25 12 4.25s6.567 0 8.204.508m-4.69 7.992-5.716 3.3v-6.6l5.715 3.3Z"
+      clipRule="evenodd"
+    />
+  </svg>
+);
+export default SvgIconBrandYoutube;
---
+++ src/IconItalic.js
@@ -0,0 +1,20 @@
+import * as React from "react";
+const SvgIconItalic = (props) => (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    width={24}
+    height={24}
+    fill="none"
+    viewBox="0 0 24 24"
+    {...props}
+   className={`justd-icons ${props.className ? props.className : "size-4"}`} data-slot="icon" aria-hidden="true">
+    <path
+      stroke="currentColor"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+      strokeWidth={1.5}
+      d="M9.75 3.75h4.75m4.75 0H14.5m0 0-5 16.5m0 0H4.75m4.75 0h4.76"
+    />
+  </svg>
+);
+export default SvgIconItalic;
