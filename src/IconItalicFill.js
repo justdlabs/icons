c1fb3e8ede2a957de8d88d2eb28057683a2d8627
Irsyad A. Panjaitan <irsyad@parsinta.com>
2024-08-18 19:22:51 +0700

    ig files

---
+++ src/IconItalicFill.js
@@ -0,0 +1,19 @@
+import * as React from "react";
+const SvgIconItalicFill = (props) => (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    width={24}
+    height={24}
+    fill="none"
+    viewBox="0 0 24 24"
+    {...props}
+   className={`justd-icons ${props.className ? props.className : "size-4"}`} data-slot="icon" aria-hidden="true">
+    <path
+      fill="currentColor"
+      fillRule="evenodd"
+      d="M9 3.75A.75.75 0 0 1 9.75 3h9.5a.75.75 0 0 1 0 1.5h-4.194l-4.545 15h3.75a.75.75 0 0 1 0 1.5H4.75a.75.75 0 0 1 0-1.5h4.194l4.545-15H9.75A.75.75 0 0 1 9 3.75"
+      clipRule="evenodd"
+    />
+  </svg>
+);
+export default SvgIconItalicFill;
---
+++ src/IconPercent.js
@@ -0,0 +1,28 @@
+import * as React from "react";
+const SvgIconPercent = (props) => (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    width={24}
+    height={24}
+    fill="none"
+    viewBox="0 0 24 25"
+    {...props}
+   className={`justd-icons ${props.className ? props.className : "size-4"}`} data-slot="icon" aria-hidden="true">
+    <path
+      stroke="currentColor"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+      strokeWidth={1.5}
+      d="m16 8.75-8 8m13.25-4a9.25 9.25 0 1 1-18.5 0 9.25 9.25 0 0 1 18.5 0"
+    />
+    <path
+      fill="currentColor"
+      stroke="currentColor"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+      strokeWidth={0.5}
+      d="M8.25 9.75a.75.75 0 1 0 1.5 0 .75.75 0 0 0-1.5 0m6 6a.75.75 0 1 0 1.5 0 .75.75 0 0 0-1.5 0"
+    />
+  </svg>
+);
+export default SvgIconPercent;
