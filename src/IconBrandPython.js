96ca08988c4b9afa9f43bf1bd7a70e998314d456
Irsyad A. Panjaitan <irsyad@parsinta.com>
2024-08-28 10:54:20 +0700

    add icons

---
+++ src/IconBrandPython.js
@@ -0,0 +1,22 @@
+import * as React from "react";
+const SvgIconBrandPython = (props) => (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    width={24}
+    height={24}
+    fill="none"
+    viewBox="0 0 24 25"
+    {...props}
+   className={`justd-icons ${props.className ? props.className : "size-4"}`} data-slot="icon" aria-hidden="true">
+    <path
+      fill="currentColor"
+      fillOpacity={0.7}
+      d="M11.428 3.25c-5.077 0-4.76 2.202-4.76 2.202l.006 2.281h4.845v.685h-6.77S1.5 8.048 1.5 13.172c0 5.122 2.836 4.94 2.836 4.94h1.692v-2.377S5.937 12.9 8.818 12.9h4.806s2.7.043 2.7-2.61V5.904s.41-2.654-4.896-2.654M8.756 4.784a.872.872 0 1 1 0 1.743.872.872 0 0 1 0-1.743"
+    />
+    <path
+      fill="currentColor"
+      d="M11.572 23.146c5.077 0 4.76-2.202 4.76-2.202l-.006-2.281h-4.845v-.685h6.77s3.249.369 3.249-4.754-2.836-4.941-2.836-4.941h-1.692v2.376s.091 2.836-2.79 2.836H9.375s-2.7-.044-2.7 2.61v4.386s-.41 2.655 4.896 2.655Zm2.672-1.534a.871.871 0 1 1-.002-1.743.871.871 0 0 1 .002 1.743"
+    />
+  </svg>
+);
+export default SvgIconBrandPython;
