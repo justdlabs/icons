e002ebcee94d5748d1a05b96fde489d159b8fbb1
Irsyad A. Panjaitan <irsyad@parsinta.com>
2025-01-05 19:57:32 +0700

    fix: update figma brand

--- src/IconBrandFigma.js
+++ src/IconBrandFigma.js
@@ -5,18 +5,15 @@ const SvgIconBrandFigma = (props) => (
     width="16"
     height="16"
     fill="none"
-    viewBox="0 0 24 24"
+    viewBox="0 0 25 24"
     {...props}
    className={`justd-icons ${props.className || "size-4"}`} data-slot={props["data-slot"] || "icon"} aria-hidden="true">
     <path
-      fill="currentColor"
-      fillRule="evenodd"
-      d="M8.665 2a3.335 3.335 0 0 0-.014 6.667 3.335 3.335 0 0 0 .014 6.666H12V12.02a3.335 3.335 0 1 0 3.347-3.354A3.335 3.335 0 0 0 15.332 2zm6.647 6.667h-3.313v3.312a3.335 3.335 0 0 1 3.313-3.312"
-      clipRule="evenodd"
-    />
-    <path
-      fill="currentColor"
-      d="M8.665 22A3.334 3.334 0 0 0 12 18.667v-3.334H8.665a3.335 3.335 0 0 0 0 6.667"
+      stroke="currentColor"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+      strokeWidth={1.286}
+      d="M12.5 3.5h-3a3 3 0 1 0 0 6m3-6v6m0-6h3a3 3 0 0 1 0 6m-6 0h3m-3 0a3 3 0 1 0 0 6m3-6h3m-3 0v6m3-6a3 3 0 1 0 0 6.001 3 3 0 0 0 0-6.001m-3 6v3a3 3 0 1 1-3-3m3 0h-3"
     />
   </svg>
 );
