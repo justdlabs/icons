0cc9c73d24f218002b81ceee66e20e3554738cc8
Irsyad A. Panjaitan <irsyad@parsinta.com>
2025-01-21 01:06:23 +0700

    wip

---
+++ src/IconPaperclip45.js
@@ -0,0 +1,19 @@
+import * as React from "react";
+const SvgIconPaperclip45 = (props) => (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    width="16"
+    height="16"
+    fill="none"
+    viewBox="0 0 25 24"
+    {...props}
+   className={`justd-icons ${props.className || "size-4"}`} data-slot={props["data-slot"] || "icon"} aria-hidden="true">
+    <path
+      stroke="currentColor"
+      strokeLinecap="round"
+      strokeWidth={1.5}
+      d="M8.843 6.657 5.661 9.839a6 6 0 0 0 0 8.485l.354.354a6 6 0 0 0 8.485 0l5.834-5.834a4.25 4.25 0 1 0-6.01-6.01l-5.57 5.568a2.125 2.125 0 1 0 3.006 3.005l5.038-5.038"
+    />
+  </svg>
+);
+export default SvgIconPaperclip45;
